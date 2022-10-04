c186a24c7c58b330642df0ae57c51f3bc6310c61
Joy Mukherjee <34181957+joymkj@users.noreply.github.com>
2022-10-04 03:27:49 -0400

    added search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,8 @@ function App() {
   const [showPopup, setShowPopup] = useState(false);
   const [cart, setCart] = useState([]);
   const [sort, setSort] = useState('Name');
+  const [search, setSearch] = useState('');
+  const [productList, setProductList] = useState(Inventory);
 
   const updateCart = (Roll) => {
     setCartSize(cartSize + 1);
@@ -31,23 +33,28 @@ function App() {
     };
   }, [cartSize]);
 
-  function renderRolls(Inventory) {
+  function renderRolls(productList) {
     return (
       <Product
-        key={Inventory.id}
-        id={Inventory.id}
-        rollName={Inventory.name}
-        rollPrice={Inventory.price}
-        rollURL={Inventory.url}
+        key={productList.id}
+        id={productList.id}
+        rollName={productList.name}
+        rollPrice={productList.price}
+        rollURL={productList.url}
         updateCart={updateCart}
       />
     );
   }
 
-  function handleSort(sortSelection) {
-    setSort(sortSelection);
-    if (sortSelection === 'Name') Inventory.sort((a, b) => (a.name > b.name ? 1 : -1));
-    else Inventory.sort((a, b) => (a.price > b.price ? 1 : -1));
+  function handleSort(event) {
+    setSort(event.target.value);
+    if (event.target.value === 'Name') productList.sort((a, b) => (a.name > b.name ? 1 : -1));
+    else productList.sort((a, b) => (a.price > b.price ? 1 : -1));
+  }
+
+  function searchButtonClicked() {
+    setProductList(Inventory.filter((str) => str.name.toLowerCase().includes(search)));
+    console.log(productList);
   }
 
   return (
@@ -55,15 +62,15 @@ function App() {
       <Navbar cartSize={cartSize} cartTotal={cartTotal} roll={latestRoll} showPopup={showPopup} />
       <div className="Gallery">
         <div className="menu">
-          <input type="text" />
-          <button>Search</button>
+          <input type="text" onChange={(e) => setSearch(e.target.value.toLowerCase())} />
+          <button onClick={searchButtonClicked}>Search</button>
           <p style={{ display: 'inline' }}>sort by: </p>
-          <select onChange={(e) => handleSort(e.target.value)}>
+          <select onChange={handleSort}>
             <option value="Name">Name</option>
             <option value="Base Price">Base Price</option>
           </select>
         </div>
-        {Inventory.map(renderRolls)}
+        {productList.map(renderRolls)}
       </div>
     </div>
   );
